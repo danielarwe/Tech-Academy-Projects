5d34c9a557a1afdfa04bebb20e905decb74abd1c
danielarwe <35775718+danielarwe@users.noreply.github.com>
2018-04-03 12:00:15 -0500

    C# Course, page 36 (methods)

---
+++ Exercise_p36_Csharp_Course/Exercise_p36_Csharp_Course/NumOps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_p36_Csharp_Course
+{
+    class NumOps
+    {
+        public static double AddFive (double num)
+        {
+            double sum = num + 5;
+            return sum;
+        }
+
+        public static double SubtractFive(double num)
+        {
+            double sum = num - 5;
+            return sum;
+        }
+
+        public static double MultiplyFive(double num)
+        {
+            double sum = num * 5;
+            return sum;
+        }
+    }
+}
---
+++ Exercise_p36_Csharp_Course/Exercise_p36_Csharp_Course/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_p36_Csharp_Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool validInput = false;
+
+            do
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.Write("Enter a number on which to do some math:  ");
+                    double num = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Your number plus 5 is equal to {NumOps.AddFive(num)}");
+                    Console.WriteLine($"Your number minus 5 is equal to {NumOps.SubtractFive(num)}");
+                    Console.WriteLine($"Your number times 5 is equal to {NumOps.MultiplyFive(num)}");
+                    validInput = true;
+                }
+
+                catch (Exception)
+                {
+                    Console.WriteLine("\nInvalid Input.  Please enter a number.\n Press any key to try again.");
+                }
+
+                finally
+                {
+                    Console.ReadKey();
+                }
+            } while (!validInput);
+       }
+    }
+}
