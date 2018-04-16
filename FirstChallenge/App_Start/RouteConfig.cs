9c0d6b967af743692f6b5aa3d4ba6dc519726d71
danielarwe <35775718+danielarwe@users.noreply.github.com>
2018-04-16 10:48:20 -0500

    ComicBookMVC-First Challenge

---
+++ FirstChallenge/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FirstChallenge
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(    //determines correct controller to turn execution over to, based on incoming URL
+                name: "Default",   //name of route
+                url: "{controller}/{action}/{id}",  //URL pattern; controller/action method to call/optional id
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );  //default controller is home controller; default action method "Index; 
+        }
+    }
+}
---
+++ FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstChallenge.Models;  //must have this to access model and data contained within
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()  //executed on call from routes.MapRoute in RouteConfig.cs
+        {
+            List<ComicBook> ComicsList = ComicBookManager.GetComicBooks();  //pull a list of comics from data model
+                                                                            //(ComicBook.cs)
+            return View(ComicsList);    //pass list of comics from data model to "Index" view (Index.cshtml)
+        }
+
+        public ActionResult Detail(int id)  //executed on call from routes.MapRoute in RouteConfig.cs
+        {                                   // takes one param., the ComicBookId to return details on correct book
+            List<ComicBook> ComicsList = ComicBookManager.GetComicBooks(); //pull a list of comics from data model
+            var comic = ComicsList.FirstOrDefault(p => p.ComicBookId == id);  //filter for matching id with input param.
+            return View(comic);  //pass ComicBook object to view for display, using "Detail" view (Detail.cshtml)
+        }
+    }
+}
